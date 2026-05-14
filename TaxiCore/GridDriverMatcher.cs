e49c0b21c05657b2213b49212a3bc2a3f484c25b
dankerSM <moiseevdanil621@gmail.com>
2026-05-15 03:47:00 +0500

    Реализованы 3 алгоритма поиска и проведены бенчмарки

---
+++ TaxiCore/GridDriverMatcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiCore
+{
+    public class GridDriverMatcher : IDriverMatcher
+    {
+        public IEnumerable<Driver> FindClosestDrivers(Coordinate orderPos, IEnumerable<Driver> allDrivers, int count = 5)
+        {
+            int cellSize = 10;
+
+
+            var grid = allDrivers.ToLookup(d => (d.Position.X / cellSize, d.Position.Y / cellSize));
+
+            int orderX = orderPos.X / cellSize;
+            int orderY = orderPos.Y / cellSize;
+
+            var candidates = new List<Driver>();
+            int radius = 0;
+
+            while (candidates.Count < count && radius < 100)
+            {
+                for (int x = orderX - radius; x <= orderX + radius; x++)
+                {
+                    for (int y = orderY - radius; y <= orderY + radius; y++)
+                    {
+
+                        if (Math.Abs(x - orderX) == radius || Math.Abs(y - orderY) == radius)
+                        {
+                            candidates.AddRange(grid[(x, y)]);
+                        }
+                    }
+                }
+                radius++;
+            }
+
+            return candidates
+                .OrderBy(d => GetDist(orderPos, d.Position))
+                .Take(count);
+        }
+
+        private double GetDist(Coordinate p1, Coordinate p2) =>
+            Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+    }
+}
---
+++ TaxiCore/RadiusDriverMatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiCore
+{
+    public class RadiusDriverMatcher : IDriverMatcher
+    {
+        public IEnumerable<Driver> FindClosestDrivers(Coordinate orderPos, IEnumerable<Driver> allDrivers, int count = 5)
+        {
+            int initialRadius = 50;
+
+            var candidates = allDrivers
+                .Where(d => Math.Abs(d.Position.X - orderPos.X) <= initialRadius &&
+                            Math.Abs(d.Position.Y - orderPos.Y) <= initialRadius)
+                .ToList();
+
+            if (candidates.Count < count)
+            {
+                candidates = allDrivers.ToList();
+            }
+
+            return candidates
+                .OrderBy(d => GetDistance(orderPos, d.Position))
+                .Take(count);
+        }
+
+        private double GetDistance(Coordinate p1, Coordinate p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
